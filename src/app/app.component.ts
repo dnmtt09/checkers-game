c9e7d751ce6376d23d61e67ea6994c6196d5a290
Daniele Matta <daniele.matta@aicof.it>
2024-06-16 19:16:54 +0200

    adding translation. To concluse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ChessboardComponent } from './components/chessboard/chessboard.component';
+
+import {
+  TranslateService,
+  TranslateModule,
+  TranslateStore,
+  TranslateLoader,
+} from '@ngx-translate/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { translationHttpLoader } from './core/translate/http-loader-factory';
+
+registerLocaleData('it');
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [CommonModule, RouterOutlet, ChessboardComponent],
+  imports: [BrowserModule, HttpClientModule, CommonModule, RouterOutlet, ChessboardComponent],
+  providers: [
+    // { provide: TranslateLoader, useClass: translationHttpLoader, deps: [HttpClient] }, // Provide the translate loader
+    { provide: TranslateService, useClass: TranslateService }, // Provide the translate service
+  ],
 })
 export class AppComponent {
   title = 'checkers-game';
+
+  constructor(private translateService: TranslateService) {
+    this.initTranslateService();
+  }
+
+  private initTranslateService(): void {
+    this.translateService.setDefaultLang('it');
+    this.translateService.use('it');
+  }
 }
