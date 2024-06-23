1e06405152c274fd167057d1785308044f45f5e6
Daniele Matta <daniele.matta@aicof.it>
2024-06-23 11:49:18 +0200

    initialize logic to get color of human player

--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+/*declare enum. This enum insert into a common file*/
+/*create an object that represents a player*/
 
 @Component({
   selector: 'app-main-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-component.component.html',
   styleUrl: './main-component.component.scss'
 })
 export class MainComponentComponent {
+  playYesOrNo: boolean = false;
+  /*player: object;
 
+  stateGame(colorPlayer: playerChoose): void {
+    this.player.color = colorPlayer;
+    this.playYesOrNo = true;
+  }*/
 }
