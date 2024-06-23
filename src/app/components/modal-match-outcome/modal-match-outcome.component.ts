efc8ec6d0d0be5709f49651fbf3f45f65e849123
Daniele Matta <daniele.matta@aicof.it>
2024-06-23 11:52:23 +0200

    create a new component

---
+++ src/app/components/modal-match-outcome/modal-match-outcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-match-outcome',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-match-outcome.component.html',
+  styleUrl: './modal-match-outcome.component.scss'
+})
+export class ModalMatchOutcomeComponent {
+
+}
