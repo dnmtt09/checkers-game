41bb3f78a88365af787d760e7e1997840d5cedab
Daniele Matta <daniele.matta@aicof.it>
2024-10-31 17:41:02 +0100

    cleanup code

--- src/app/services/PcService.ts
+++ src/app/services/PcService.ts
@@ -13,27 +13,7 @@ export class PcService {
   //TODO gestione in modalita' asincrona
 
   pcTurn(humanId: number) {
-    let stop = false;
-    this.chessboard.map((chess, index) => {
-      //TODO quando realizzo con RXJS, non ho accesso all'indice
 
-      if (chess === this.getInfo.color && !stop) {
-        if (this.isCapture(index)) {
-          if (this.isFree(index, 7)) {
-            this.chessboard[index] = '';
-            this.chessboard[index + 7] = this.getInfo.color;
-            stop = true;
-          } else if (this.isFree(index, 9)) {
-            this.chessboard[index] = '';
-            this.chessboard[index + 9] = this.getInfo.color;
-            stop = true;
-          }
-        } else {
-          this.randomMove();
-          stop = true;
-        }
-      }
-    });
   }
 
   set setInfo(color: Color) {
@@ -49,46 +29,5 @@ export class PcService {
 
   set humanColor(color: Color) {
     this._humanColor = color;
-  }
-
-  private isFree(id: number, nextPosition: 7 | 9) {
-    console.log('stampo se sei libero. ', this.chessboard[id + nextPosition]);
-    return this.chessboard[id + nextPosition] === 'white';
-  }
-  private isCapture(id: number) {
-    return (
-      this.chessboard[id + 14] === this._humanColor ||
-      this.chessboard[id + 18] === this._humanColor
-    );
-  }
-
-  private prova() {
-    for (let i = 0; i < 8; i++) {
-      console.log('\n');
-    }
-    for (let y = 0; y < 8; y++) {
-      console.log('|');
-      console.log(this.chessboard[y]);
-    }
-  }
-
-  private randomMove() {
-    let stop = false;
-    console.log('valore del colore in randomMove', this.getInfo.color);
-    this.chessboard.map((chess, index) => {
-      //TODO quando realizzo con RXJS, non ho accesso all'indice
-
-      if (chess === this.getInfo.color && !stop) {
-        if (this.isFree(index, 7)) {
-          this.chessboard[index] = '';
-          this.chessboard[index + 7] = this.getInfo.color;
-          stop = true;
-        } else if (this.isFree(index, 9)) {
-          this.chessboard[index] = '';
-          this.chessboard[index + 9] = this.getInfo.color;
-          stop = true;
-        }
-      }
-    });
   }
 }
