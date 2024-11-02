2264f1caf4affa13b29c680de9325be6200188ee
Daniele Matta <daniele.matta@aicof.it>
2024-11-02 13:04:18 +0100

    add translate module and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule, registerLocaleData } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { ChessboardComponent } from './components/chessboard/chessboard.component';
+import {Component} from '@angular/core';
+import {CommonModule, registerLocaleData} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {ChessboardComponent} from './components/chessboard/chessboard.component';
 
-import {
-  TranslateService,
-  TranslateModule,
-  TranslateStore,
-  TranslateLoader,
-} from '@ngx-translate/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { translationHttpLoader } from './core/translate/http-loader-factory';
+import {TranslateModule, TranslateService,} from '@ngx-translate/core';
 
 registerLocaleData('it');
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  imports: [BrowserModule, HttpClientModule, CommonModule, RouterOutlet, ChessboardComponent],
-  providers: [
-    // { provide: TranslateLoader, useClass: translationHttpLoader, deps: [HttpClient] }, // Provide the translate loader
-    { provide: TranslateService, useClass: TranslateService }, // Provide the translate service
-  ],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    imports: [CommonModule, RouterOutlet, ChessboardComponent, TranslateModule],
 })
 export class AppComponent {
-  title = 'checkers-game';
+    title = 'checkers-game';
 
-  constructor(private translateService: TranslateService) {
-    this.initTranslateService();
-  }
+    constructor(private translateService: TranslateService) {
+        this.initTranslateService();
+    }
 
-  private initTranslateService(): void {
-    this.translateService.setDefaultLang('it');
-    this.translateService.use('it');
-  }
+    private initTranslateService(): void {
+        this.translateService.setDefaultLang('it');
+        this.translateService.use('en');
+    }
 }
