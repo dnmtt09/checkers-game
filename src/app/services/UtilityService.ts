741be9dca59c3aac7f4fda907fafa809036fcb76
Daniele Matta <daniele.matta@aicof.it>
2024-11-02 13:37:08 +0100

    cleanup code

--- src/app/services/UtilityService.ts
+++ src/app/services/UtilityService.ts
@@ -30,34 +30,8 @@ export class UtilityService {
         this.chessboard[humanPieces[i]] = human.color;
       }
     }
-    //   for (let i = 0; i < this.totalChess; i++) {
-    //     if (i % 8 === 0) {
-    //       this.isOdd = !this.isOdd;
-    //     }
-    //     if (this.isOdd) {
-    //       this.chessboard.push(i % 2 === 0 ? 'black' : 'white');
-    //     } else {
-    //       this.chessboard.push(i % 2 === 0 ? 'white' : 'black');
-    //     }
-    //   }
-    //
-    //   for (let i = 0; i < 8; i = i + 2) {
-    //     /* TODO first PC */
-    //     this.chessboard[i] = PC.color;
-    //   }
-    //   for (let i = 9; i < 16; i = i + 2) {
-    //     /* TODO second PC */
-    //     this.chessboard[i] = PC.color;
-    //   }
-    //   for (let i = 48; i < 56; i = i + 2) {
-    //     /* TODO second HUMAN */
-    //     this.chessboard[i] = human.color;
-    //   }
-    //   for (let i = 57; i < 64; i = i + 2) {
-    //     /* TODO second HUMAN */
-    //     this.chessboard[i] = human.color;
-    //   }
-    // }
+
+
 
   }
 
